0fbd95b128e0377f586c3b55b552f2f66a03d617
𝙳𝙰𝚁𝙺 𝙲𝙾𝚁𝙴 <126108471+Manuel12yt@users.noreply.github.com>
2025-01-22 18:48:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import { dlpinterest } from './lib/img-pinterest.js';
+import { dlgato } from './lib/img-gato.js';
+import { dlpato } from './lib/img-pato.js';
+import { dlneko } from './lib/img-pato.js';
+
+// Exportando todos los elementos
+export { dlgato, dlpinterest, dlpato, dlneko };
